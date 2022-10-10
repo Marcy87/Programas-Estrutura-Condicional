6f3feb8b6a4afb7086ebf98b20e873ba2a5db27b
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-10 17:57:19 -0300

    Lanche
    Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A seguir, calcule e mostre o valor da conta a pagar.
    
    Código    Especificação              Preço
    1              Cachorro Quente       R$ 4.00
    2              X - Salada                  R$ 4.50
    3              X-Bacon                     R$ 5.00
    4              Torrada simples         R$ 2.00
    5              Refrigerante              R$ 1.50
    
    Entrada
    O arquivo de entrada contém dois valores inteiros correspondentes ao código e à quantidade de um item conforme tabela acima.
    
    Saída
    O arquivo de saída deve conter a mensagem "Total: R$ " seguido pelo valor a ser pago, com 2 casas após o ponto decimal.

---
+++ Program1038/Program1038/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1038
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] vet = Console.ReadLine().Split(' ');
+
+            int cod = int.Parse(vet[0]);
+            int qtd = int.Parse(vet[1]);
+
+            double total;
+
+            if (cod == 1)
+            {
+                total = qtd * 4.0;
+            }
+            else if (cod == 2)
+            {
+                total = qtd * 4.5;
+            }
+            else if (cod == 3)
+            {
+                total = qtd * 5.0;
+            }
+            else if (cod == 4)
+            {
+                total = qtd * 2.0;
+            }
+            else
+            {
+                total = qtd * 1.5;
+            }
+            Console.WriteLine("Total: R$ " + total.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
