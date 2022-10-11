26160849e18c06f135f01b1a44c0019154a710cd
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-11 17:44:30 -0300

    Tempo de Jogo com Minutos
    Leia a hora inicial, minuto inicial, hora final e minuto final de um jogo. A seguir calcule a duração do jogo.
    
    Obs: O jogo tem duração mínima de um (1) minuto e duração máxima de 24 horas.
    
    Entrada
    Quatro números inteiros representando a hora de início e fim do jogo.
    
    Saída
    Mostre a seguinte mensagem: “O JOGO DUROU XXX HORA(S) E YYY MINUTO(S)” .

---
+++ Program1047/Program1047/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program1047
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int horaInicial = int.Parse(valores[0]);
+            int minutosInicial = int.Parse(valores[1]);
+            int horaFinal = int.Parse(valores[2]);
+            int minutosFinal = int.Parse(valores[3]);
+
+            int instanteInicial = horaInicial * 60 + minutosInicial;
+            int instanteFinal = horaFinal * 60 + minutosFinal;
+
+            int duracao;
+
+            if (instanteInicial < instanteFinal)
+            {
+                duracao = instanteFinal - instanteInicial;
+            }
+            else
+            {
+                duracao = (24 * 60 - instanteInicial) + instanteFinal;
+            }
+
+            int duracaoHoras = duracao / 60;
+            int duracaoMinutos = duracao % 60;
+
+            Console.WriteLine("O JOGO DUROU " + duracaoHoras + " HORA(S) E " + duracaoMinutos + " MINUTO(S)");
+        }
+    }
+}
