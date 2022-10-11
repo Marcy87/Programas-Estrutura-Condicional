a694b72aa997775882c2d22d700a29f76e8ff4f7
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-11 14:34:26 -0300

    Triângulo
    Leia 3 valores reais (A, B e C) e verifique se eles formam ou não um triângulo. Em caso positivo, calcule o perímetro do triângulo e apresente a mensagem:
    
    
    Perimetro = XX.X
    
    
    Em caso negativo, calcule a área do trapézio que tem A e B como base e C como altura, mostrando a mensagem
    
    
    Area = XX.X
    
    Entrada
    A entrada contém três valores reais.
    
    Saída
    O resultado deve ser apresentado com uma casa decimal.

---
+++ Program1043/Program1043/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1043
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double perimetro, area;
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            double A = double.Parse(valores[0], CultureInfo.InvariantCulture);
+            double B = double.Parse(valores[1], CultureInfo.InvariantCulture);
+            double C = double.Parse(valores[2], CultureInfo.InvariantCulture);
+
+            if (A < B + C && B < A + C && C < A + B)
+            {
+                perimetro = A + B + C;
+                Console.WriteLine("Perimetro = " + perimetro.ToString("F1", CultureInfo.InvariantCulture));
+            }
+            else
+            {
+                area = ((A + B) * C) / 2;
+                Console.WriteLine("Area = " + area.ToString("F1", CultureInfo.InvariantCulture));
+            }
+        }
+    }
+}
