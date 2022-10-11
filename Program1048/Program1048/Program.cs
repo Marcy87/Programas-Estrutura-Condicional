4d96b8dd95bb1e21df7223f7a0b0724c667e9662
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-11 19:34:26 -0300

    Aumento de Salário
    A empresa ABC resolveu conceder um aumento de salários a seus funcionários de acordo com a tabela abaixo:
    
    
    Salário	
    0 - 400.00
    400.01 - 800.00
    800.01 - 1200.00
    1200.01 - 2000.00
    Acima de 2000.00
    
    Percentual de Reajuste
    15%
    12%
    10%
    7%
    4%
    
    Leia o salário do funcionário e calcule e mostre o novo salário, bem como o valor de reajuste ganho e o índice reajustado, em percentual.
    
    Entrada
    A entrada contém apenas um valor de ponto flutuante, com duas casas decimais.
    
    Saída
    Imprima 3 linhas na saída: o novo salário, o valor ganho de reajuste (ambos devem ser apresentados com 2 casas decimais) e o percentual de reajuste ganho, conforme exemplo abaixo.

---
+++ Program1048/Program1048/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1048
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double aumento, novoSalario, percentual;
+
+            double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (salario <= 400.00)
+            {
+                percentual = 15.0;
+            }
+            else if (salario <= 800.00)
+            {
+                percentual = 12.0;
+            }
+            else if (salario <= 1200.00)
+            {
+                percentual = 10.0;
+            }
+            else if (salario <= 2000.00)
+            {
+                percentual = 7.0;
+            }
+            else
+            {
+                percentual = 4.0;
+            }
+
+            aumento = salario * percentual / 100.00;
+            novoSalario = salario + aumento;
+
+            Console.WriteLine("Novo salario: " + novoSalario.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Reajuste ganho: " + aumento.ToString("F2"), CultureInfo.InvariantCulture);
+            Console.WriteLine("Em percentual: " + percentual + " %");
+        }
+    }
+}
