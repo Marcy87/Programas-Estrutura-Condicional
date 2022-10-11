540b085913a5c62e259fa281e089439a8f848fd9
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-11 15:59:31 -0300

    Tempo de Jogo
    Leia a hora inicial e a hora final de um jogo. A seguir calcule a duração do jogo, sabendo que o mesmo pode começar em um dia e terminar em outro, tendo uma duração mínima de 1 hora e máxima de 24 horas.
    
    Entrada
    A entrada contém dois valores inteiros representando a hora de início e a hora de fim do jogo.
    
    Saída
    Apresente a duração do jogo conforme exemplo abaixo.

---
+++ Program1046/Program1046/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Program1046
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int duracao;
+
+            string[] valores = Console.ReadLine().Split(' ');
+
+            int horaInicial = int.Parse(valores[0]);
+            int horaFinal = int.Parse(valores[1]);
+
+            if (horaInicial < horaFinal)
+            {
+                duracao = horaFinal - horaInicial;
+            } else
+            {
+                duracao = 24 - horaInicial + horaFinal;                
+            }
+            Console.WriteLine("O JOGO DUROU " + duracao + " HORA(S)");
+        }
+    }
+}
