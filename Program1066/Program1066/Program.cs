843e9db7252c69b5d32fa49c12f290cfc628603e
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-13 18:38:24 -0300

    Pares, Ímpares, Positivos e Negativos
    Leia 5 valores Inteiros. A seguir mostre quantos valores digitados foram pares, quantos valores digitados foram ímpares, quantos valores digitados foram positivos e quantos valores digitados foram negativos.
    
    Entrada
    O arquivo de entrada contém 5 valores inteiros quaisquer.
    
    Saída
    Imprima a mensagem conforme o exemplo fornecido, uma mensagem por linha, não esquecendo o final de linha após cada uma.

---
+++ Program1066/Program1066/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+
+namespace Program1066
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+
+            int par = 0, impar = 0, positivo = 0, negativo = 0;
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+            if (num > 0)
+            {
+                positivo++;
+            }
+            else if (num < 0)
+            {
+                negativo++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+            if (num > 0)
+            {
+                positivo++;
+            }
+            else if (num < 0)
+            {
+                negativo++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+            if (num > 0)
+            {
+                positivo++;
+            }
+            else if (num < 0)
+            {
+                negativo++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+            if (num > 0)
+            {
+                positivo++;
+            }
+            else if (num < 0)
+            {
+                negativo++;
+            }
+
+            num = int.Parse(Console.ReadLine());
+            if (num % 2 == 0)
+            {
+                par++;
+            }
+            else
+            {
+                impar++;
+            }
+            if (num > 0)
+            {
+                positivo++;
+            }
+            else if (num < 0)
+            {
+                negativo++;
+            }
+
+            Console.WriteLine(par + " valor(es) par(es)");
+            Console.WriteLine(impar + " valor(es) impar(es)");
+            Console.WriteLine(positivo + " valor(es) positivo(s)");
+            Console.WriteLine(negativo + " valor(es) negativo(s)");
+        }
+    }
+}
