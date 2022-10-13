8d1e2881f9050849f60f0d50d21ba69dae94332c
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-13 18:02:57 -0300

    Pares entre Cinco Números
    Faça um programa que leia 5 valores inteiros. Conte quantos destes valores digitados são pares e mostre esta informação.
    
    Entrada
    O arquivo de entrada contém 5 valores inteiros quaisquer.
    
    Saída
    Imprima a mensagem conforme o exemplo fornecido, indicando a quantidade de valores pares lidos.

---
+++ Program1065/Program1065/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Program1065
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+            int n4 = int.Parse(Console.ReadLine());
+            int n5 = int.Parse(Console.ReadLine());
+
+            int conta = 0;
+
+            if (n1 % 2 == 0)
+            {
+                conta++;
+            }
+            if (n2 % 2 == 0)
+            {
+                conta++;
+            }
+            if (n3 % 2 == 0)
+            {
+                conta++;
+            }
+            if (n4 % 2 == 0)
+            {
+                conta++;
+            }
+            if (n5 % 2 == 0)
+            {
+                conta++;
+            }
+
+            Console.WriteLine(conta + " valores pares");
+        }
+    }
+}
