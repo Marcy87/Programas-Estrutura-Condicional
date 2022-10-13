e6318ff99f5aa5700e1553cb6b94b8d62e049c5d
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-13 15:37:06 -0300

    Números Positivos
    Faça um programa que leia 6 valores. Estes valores serão somente negativos ou positivos (desconsidere os valores nulos). A seguir, mostre a quantidade de valores positivos digitados.
    
    Entrada
    Seis valores, negativos e/ou positivos.
    
    Saída
    Imprima uma mensagem dizendo quantos valores positivos foram lidos.

---
+++ Program1060/Program1060/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1060
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1, n2, n3, n4, n5, n6;
+            int conta = 0;
+
+            n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n5 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            n6 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            if (n1 > 0.0)
+            {
+                conta++;
+            }
+            if (n2 > 0.0)
+            {
+                conta++;
+            }
+            if (n3 > 0.0)
+            {
+                conta++;
+            }
+            if (n4 > 0.0)
+            {
+                conta++;
+            }
+            if (n5 > 0.0)
+            {
+                conta++;
+            }
+            if (n6 > 0.0)
+            {
+                conta++;
+            }
+            Console.WriteLine(conta + " valores positivos");
+        }
+    }
+}
