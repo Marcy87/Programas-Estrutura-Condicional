dca52a7901cd73125d330565a9ffe3571f0c3ed2
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-13 17:56:41 -0300

    Positivos e Média
    Leia 6 valores. Em seguida, mostre quantos destes valores digitados foram positivos. Na próxima linha, deve-se mostrar a média de todos os valores positivos digitados, com um dígito após o ponto decimal.
    
    Entrada
    A entrada contém 6 números que podem ser valores inteiros ou de ponto flutuante. Pelo menos um destes números será positivo.
    
    Saída
    O primeiro valor de saída é a quantidade de valores positivos. A próxima linha deve mostrar a média dos valores positivos digitados.

---
+++ Program1064/Program1064/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+
+namespace Program1064
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n1 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double n2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double n3 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double n4 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double n5 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double n6 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            int conta = 0;
+            double valor = 0, media;
+
+            if (n1 > 0.0)
+            {
+                conta++;
+                valor += n1;
+            }
+            if (n2 > 0.0)
+            {
+                conta++;
+                valor += n2;
+            }
+            if (n3 > 0.0)
+            {
+                conta++;
+                valor += n3;
+
+            }
+            if (n4 > 0.0)
+            {
+                conta++;
+                valor += n4;
+
+            }
+            if (n5 > 0.0)
+            {
+                conta++;
+                valor += n5;
+
+            }
+            if (n6 > 0.0)
+            {
+                conta++;
+                valor += n6;
+
+            }
+
+            media = valor / conta;
+            
+            Console.WriteLine(conta + " valores positivos");
+            Console.WriteLine(media.ToString("F1", CultureInfo.InvariantCulture));
+        }
+    }
+}
