5744012036bfae3f9f392cbaf96c6fd5cb69c1c6
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-13 14:24:36 -0300

    DDD
    Leia um número inteiro que representa um código de DDD para discagem interurbana. Em seguida, informe à qual cidade o DDD pertence, considerando a tabela abaixo:
    
    61 - Brasilia;
    71 - Salvador;
    11 - Sao Paulo;
    21 - Rio de Janeiro;
    32 - Juiz de Fora;
    19 - Campinas;
    27 - Vitoria;
    31 - Belo Horizonte.
    
    Se a entrada for qualquer outro DDD que não esteja presente na tabela acima, o programa deverá informar:
    DDD nao cadastrado
    
    Entrada
    A entrada consiste de um único valor inteiro.
    
    Saída
    Imprima o nome da cidade correspondente ao DDD existente na entrada. Imprima DDD nao cadastrado caso não existir DDD correspondente ao número digitado.

---
+++ Program1050/Program1050/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Program1050
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cod = int.Parse(Console.ReadLine());
+
+            if (cod == 61)
+            {
+                Console.WriteLine("Brasilia");
+            }
+            else if (cod == 71)
+            {
+                System.Console.WriteLine("Salvador");
+            }
+            else if (cod == 11)
+            {
+                System.Console.WriteLine("Sao Paulo");
+            }
+            else if (cod == 21)
+            {
+                System.Console.WriteLine("Rio de Janeiro");
+            }
+            else if (cod == 32)
+            {
+                System.Console.WriteLine("Juiz de Fora");
+            }
+            else if (cod == 19)
+            {
+                System.Console.WriteLine("Campinas");
+            }
+            else if (cod == 27)
+            {
+                System.Console.WriteLine("Vitoria");
+            }
+            else if (cod == 31)
+            {
+                System.Console.WriteLine("Belo Horizonte");
+            }
+            else
+            {
+                Console.WriteLine("DDD nao cadastrado");
+            }
+        }
+    }
+}
