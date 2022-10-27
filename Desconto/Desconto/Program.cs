5294d91600540b3fa853cecff97eb2ce67236f54
Marcello Di Silvestro <marcellodisilvestro@outlook.com>
2022-10-27 19:17:15 -0300

    Desconto com a Expressão Condicional Ternária

---
+++ Desconto/Desconto/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Globalization;
+
+namespace Desconto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            double desconto = (preco < 20.0) ? preco * 0.1 : preco * 0.05;
+
+            Console.WriteLine(desconto.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
